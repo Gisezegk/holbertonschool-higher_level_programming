0bef3971f6e1ffff3dd5bf07deed28e36bed3393
Gisezegk <anatanonamewanamedesuka2@gmail.com>
2023-06-03 00:58:44 -0700

    comb3

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -2,8 +2,7 @@
 for i in range(10):
     for n in range(i + 1, 10):
         if i != n:
-            if (i+n) != 19:
+            if (i+n) != 17:
                 print("{}{}".format(i, n), end=", ")
-            elif (i+n) == 19:
+            elif (i+n) == 17:
                 print("89")
-
