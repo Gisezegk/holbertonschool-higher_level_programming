8218fee8b5d858a66794413e61811799e681917e
Gisezegk <anatanonamewanamedesuka2@gmail.com>
2023-06-03 01:12:47 -0700

    uppercase

---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+#!/usr/bin/python3
+def uppercase(str):
+    for i in str:
+        i = ord(i)
+        if i in range(96, 123):
+            i -= 32
+        print("{}".format(chr(i)), end="")
+    print()
