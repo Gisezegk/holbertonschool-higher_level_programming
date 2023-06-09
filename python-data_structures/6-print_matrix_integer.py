e6406eb834a29c42bdeb4c7c28754721ac967f3a
Gisezegk <anatanonamewanamedesuka2@gmail.com>
2023-06-09 00:24:10 -0700

    print matrizx integer

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
-        for num in row:
+        for i, num in enumerate(row, start=1):
+            if i != 1:
+                print("{}".format(" "), end="")
             print("{:d}".format(num), end=" ")
         print()
