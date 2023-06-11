afb419b83cbf98333b4e749ccc23efa02d62321d
Gisezegk <anatanonamewanamedesuka2@gmail.com>
2023-06-11 16:35:11 -0700

    task 2

--- python-data_structures/2-replace_in_list.py
+++ python-data_structures/2-replace_in_list.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
+   
     new_list = my_list
+    
+
     for i in my_list:
-        if idx < 0:
-            return (None)
-        if idx < i:         
-            return (None)
+        if idx > len(my_list) - 1 or idx < 0:
+            return my_list
+
         if my_list[i] == my_list[idx]:
             new_list[i] = element
             return new_list
