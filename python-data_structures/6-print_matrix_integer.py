dc2f63d981ec4015a885eb828e2b8285ba0d6ccf
Gisezegk <anatanonamewanamedesuka2@gmail.com>
2023-06-11 16:21:46 -0700

    task 6

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
+
+
     for row in matrix:
         for i, num in enumerate(row, start=1):
             if i != 1:
                 print("{}".format(" "), end="")
-            print("{:d}".format(num), end=" ")
+            print("{:d}".format(num), end="")
         print()
