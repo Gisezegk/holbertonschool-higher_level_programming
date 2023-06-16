c8aa03d1c8c1014d5fa383bc2cc00089f07ae42d
Gisezegk <anatanonamewanamedesuka2@gmail.com>
2023-06-15 21:48:33 -0700

    size validtio

---
+++ python-classes/2-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""New square"""
+
+
+class Square:
+    """Define a square"""
+    def __init__(self, size=0) -> None:
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+        elif not isinstance(size, int):
+            raise TypeError("size must be an integer")
+
+        else:
+            self.__size = size
+        pass
