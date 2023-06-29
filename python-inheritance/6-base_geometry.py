4acc69c87ed5ac1c2fc1427f225caa4af1ff10c7
Gisezegk <anatanonamewanamedesuka2@gmail.com>
2023-06-28 19:08:51 -0700

    improve Geometry

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Empty class
+"""
+
+
+class BaseGeometry:
+    """
+    not more empty geometry
+    """
+    def area(self):
+        raise Exception("area() is not implemented")
