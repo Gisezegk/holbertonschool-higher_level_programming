22dd591924fbae5adf31f0fabbb4c3c18a726ce9
Gisezegk <anatanonamewanamedesuka2@gmail.com>
2023-06-28 18:00:51 -0700

    mylist

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+print_sorted modulo
+"""
+
+
+
+class MyList(list):
+    """
+    My list class
+    """
+    def print_sorted(self):
+        """
+        print
+        """
+        print(sorted(self))
