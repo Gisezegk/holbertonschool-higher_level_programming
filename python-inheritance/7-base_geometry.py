16bd3fde022c242aa446a7c40bc4dbc23dfee18d
Gisezegk <anatanonamewanamedesuka2@gmail.com>
2023-06-28 19:15:59 -0700

    integer validator

---
+++ python-inheritance/7-base_geometry.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Empty class
+"""
+
+
+class BaseGeometry:
+    """
+    Base Geometry
+    """
+    def area(self):
+        raise Exception("are() is not implemented")
+
+    def integer_validator(self, name, value):
+        if type(value) != int:
+            raise TypeError("{} must be an integer".format(name))
+        if value < 1:
+            raise ValueError("{} must be greater than 0".format(name))
