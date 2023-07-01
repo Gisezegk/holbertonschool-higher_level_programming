99c80d26d3df0ae5a3e320df3d79e0e65cfedf0f
Gisezegk <anatanonamewanamedesuka2@gmail.com>
2023-07-01 02:02:55 -0700

    codestyle

--- python-more_classes/8-rectangle.py
+++ python-more_classes/8-rectangle.py
@@ -66,7 +66,7 @@ class Rectangle:
             raise TypeError("rect_1 must be an instance of Rectangle")
         if not isinstance(rect_2, Rectangle):
             raise TypeError("rect_2 must be an instance of Rectangle")
-        
+
         area_rect_1 = rect_1.area()
         area_rect_2 = rect_2.area()
 
