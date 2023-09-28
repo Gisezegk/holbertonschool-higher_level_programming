6f6470451164b0c0f9a493abd6318b2fffaf0b0e
Gisezegk <anatanonamewanamedesuka2@dominio.com>
2023-09-27 20:54:35 -0700

    0-add.py

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    a = 1
-    b = 2
+a = 1
+b = 2
 
-    from add_0 import add
+from add_0 import add
 
-    print("{} + {} = {}". format(a, b, add(a, b)))
+result = add(a, b)
+
+print(f"{a} + {b} = {result}")
