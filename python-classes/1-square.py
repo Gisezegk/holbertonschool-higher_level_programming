cef46f4aefe5f2d6be13ba65a3e821b4721b31b1
Gisezegk <anatanonamewanamedesuka2@dominio.com>
2023-09-30 01:40:13 -0700

    1-square.py

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -6,5 +6,14 @@ Class Square that defines a square
 
 
 class Square:
+    """
+    Create an objet of class Square
+    """
     def __init__(self, size):
+        """
+        Initialize square
+
+        Attributes:
+        size: private attribute size of square
+        """
         self.__size = size
