77025716cc670680b899fdad5479993c51702a70
Gisezegk <anatanonamewanamedesuka2@gmail.com>
2024-01-28 04:35:23 +0000

    6-print_comb3.py

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -5,4 +5,7 @@ for i in range(0, 10):
             continue
         if i > g:
             continue
+        if i == 8 and g == 9:
+            print("89")
+            break
         print("{}{}".format(i, g), end=", ")
