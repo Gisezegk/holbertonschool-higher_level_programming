ef7a4a809b29283543f191ef103eb61c029c2b99
Gisezegk <anatanonamewanamedesuka2@gmail.com>
2024-02-01 15:16:47 +0000

    1-search_replace.py

---
+++ python-more_data_structures/1-search_replace.py
@@ -0,0 +1,7 @@
+#!/bin/usr/python3
+def search_replace(my_list, search, replace):
+    new_ls = my_list.copy()
+    for i, number in enumerate(new_ls):
+        if number == search:
+            new_ls[i] = replace
+    return new_ls
