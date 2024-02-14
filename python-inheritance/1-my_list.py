8fe536d77cf7209a02ee10e5514e51695f5d52aa
Gisezegk <anatanonamewanamedesuka2@gmail.com>
2024-02-14 13:42:30 +0000

    1-my_list.py

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,13 +1,9 @@
 #!/usr/bin/python3
-"""
-Module 1-mylist
-"""
-
+"""Module 1-mylist"""
 
 class MyList(list):
-    """
-    class that inherits from list
-    """
+    """class that inherits from list"""
+    
     def print_sorted(self):
         if hasattr(self, '__str__'):
             print(sorted(self))
