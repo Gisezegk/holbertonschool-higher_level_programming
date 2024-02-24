2c9327f9c32f843e419e5b784b1d8d608c6aa5ba
Gisezegk <anatanonamewanamedesuka2@gmail.com>
2024-02-24 23:22:52 +0000

    unitest

--- python-almost_a_circle/tests/test_base.py
+++ python-almost_a_circle/tests/test_base.py
@@ -1,54 +1,47 @@
 #!/usr/bin/python3
-"""
-test module for class Base
-"""
+
 import unittest
 from models.base import Base
+from models.rectangle import Rectangle
+from models.square import Square
 
-class Base(unittest.TestCase):
-    def test_init_with_id(self):
-        obj = Base(42)
-        self.assertEqual(obj.id, 42)
 
-    def test_init_without_id(self):
-        obj = Base()
-        self.assertEqual(obj.id, Base._Base__nb_objects)
+class TestBase(unittest.TestCase):
+    def test_base_creation(self):
+        b1 = Base()
+        b2 = Base()
+        self.assertEqual(b1.id, 1)
+        self.assertEqual(b2.id, 2)
 
-    def test_to_json_string_empty_list(self):
-        json_str = Base.to_json_string([])
-        self.assertEqual(json_str, "[]")
+    def test_base_custom_id(self):
+        b = Base(10)
+        self.assertEqual(b.id, 10)
 
-    def test_to_json_string_with_data(self):
-        data = [{'key': 'value'}]
-        json_str = Base.to_json_string(data)
-        self.assertEqual(json_str, '[{"key": "value"}]')
 
-    def test_save_to_file(self):
-        obj = Base()
-        Base.save_to_file([obj])
-        with open('Base.json', 'r', encoding='utf-8') as f:
-            json_str = f.read()
-        self.assertEqual(json_str, '[{"id": 1}]')
+class TestRectangle(unittest.TestCase):
+    def test_rectangle_creation(self):
+        r = Rectangle(5, 10)
+        self.assertEqual(r.width, 5)
+        self.assertEqual(r.height, 10)
 
-    def test_from_json_string_empty_string(self):
-        data = Base.from_json_string('')
-        self.assertEqual(data, [])
+    def test_rectangle_area(self):
+        r = Rectangle(5, 10)
+        self.assertEqual(r.area(), 50)
 
-    def test_from_json_string_with_data(self):
-        json_str = '[{"key": "value"}]'
-        data = Base.from_json_string(json_str)
-        self.assertEqual(data, [{'key': 'value'}])
+    # Add more tests as needed
 
-    def test_create_rectangle(self):
-        rect = Base.create(id=1, width=3, height=4)
-        self.assertEqual(rect.id, 1)
-        self.assertEqual(rect.width, 3)
-        self.assertEqual(rect.height, 4)
 
-    def test_create_square(self):
-        square = Base.create(id=2, size=5)
-        self.assertEqual(square.id, 2)
-        self.assertEqual(square.size, 5)
+class TestSquare(unittest.TestCase):
+    def test_square_creation(self):
+        s = Square(5)
+        self.assertEqual(s.size, 5)
+
+    def test_square_area(self):
+        s = Square(5)
+        self.assertEqual(s.area(), 25)
+
+    # Add more tests as needed
+
 
 if __name__ == '__main__':
     unittest.main()
